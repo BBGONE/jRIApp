88b6c9b23acb02ec7447dad0c956fc4dd9d39aba
Maxim V. Tsapov <lptscsi@gmail.com>
2016-11-16 13:16:38 +0300

    added GZIP optimization to the DEMO app

---
+++ DEMOS/RIAppDemoMVC/RIAppDemo/App_Start/BundleConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Optimization;
+using RIAppDemo.Filters;
+
+namespace RIAppDemo
+{
+    public static class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new GZipScriptBundle("~/bundles/jriapp", new JsMinify()).Include("~/Scripts/jriapp/jriapp_shared.js", "~/Scripts/jriapp/jriapp.js", 
+                "~/Scripts/jriapp/jriapp_db.js", "~/Scripts/jriapp/jriapp_ui.js", "~/Scripts/jriapp/jriapp_langs.js"));
+            bundles.Add(new GZipScriptBundle("~/bundles/shared", new JsMinify()).Include("~/Scripts/demo/shared/shared.js"));
+        }
+
+    }
+}
--- DEMOS/RIAppDemoMVC/RIAppDemo/Global.asax.cs
+++ DEMOS/RIAppDemoMVC/RIAppDemo/Global.asax.cs
@@ -4,12 +4,11 @@ using System.Threading;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace RIAppDemo
 {
-    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=9394801
     public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
@@ -19,6 +18,9 @@ namespace RIAppDemo
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            BundleTable.EnableOptimizations = true;
         }
 
         protected void Application_AuthenticateRequest(object sender, EventArgs e)
