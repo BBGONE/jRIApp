2bf39bb879b47c74f5f17f8576e3978ed85a4770
Maxim V. Tsapov <lptscsi@gmail.com>
2019-03-27 13:30:21 +0300

    Template improvements
    Now, the container for template content can be provided externally. The redundant container is not added, when it is not needed

--- FRAMEWORK/CLIENT/addins/ui/jriapp_ui/datagrid/cells/details.ts
+++ FRAMEWORK/CLIENT/addins/ui/jriapp_ui/datagrid/cells/details.ts
@@ -27,7 +27,7 @@ export class DetailsCell extends BaseObject {
             return;
         }
         this._row.tr.appendChild(this._td);
-        this._template = createTemplate(null, null);
+        this._template = createTemplate({ parentEl: null });
         this._template.templateID = options.details_id;
         this._td.appendChild(this._template.el);
     }
