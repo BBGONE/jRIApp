3c1b38bfcca00301a4c37a3d94e23d1aac77f383
Maxim T <lptscsi@gmail.com>
2019-09-11 19:49:55 +0300

    Parser improvements

--- FRAMEWORK/CLIENT/built/jriapp.d.ts
+++ FRAMEWORK/CLIENT/built/jriapp.d.ts
@@ -1080,5 +1080,5 @@ declare module "jriapp" {
     export { PropWatcher } from "jriapp/utils/propwatcher";
     export { ViewModel, BaseCommand, Command, ICommand } from "jriapp/mvvm";
     export { Application } from "jriapp/app";
-    export const VERSION = "2.23.7";
+    export const VERSION = "2.23.8";
 }
