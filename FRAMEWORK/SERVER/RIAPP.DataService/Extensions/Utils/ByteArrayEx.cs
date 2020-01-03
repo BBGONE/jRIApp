39576041508d65176380c793d7c12bfb08ff5943
Maxim T <lptscsi@gmail.com>
2020-01-03 12:17:38 +0300

    better Binary conversion
    less memory allocations

--- FRAMEWORK/SERVER/RIAPP.DataService/Extensions/Utils/ByteArrayEx.cs
+++ FRAMEWORK/SERVER/RIAPP.DataService/Extensions/Utils/ByteArrayEx.cs
@@ -1,9 +1,20 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 
 namespace RIAPP.DataService.Utils.Extensions
 {
     public static class ByteArrayEx
     {
+        static readonly string[] BYTES_MAP = new string[256];
+
+        static ByteArrayEx()
+        {
+            for (int i = 0; i < 256; ++i)
+            {
+                BYTES_MAP[i] = i.ToString();
+            }
+        }
+
         public static string ConvertToString(this byte[] bytes)
         {
             if (bytes == null)
@@ -11,20 +22,50 @@ namespace RIAPP.DataService.Utils.Extensions
                 return null;
             }
 
-            var sb = new StringBuilder(bytes.Length * 4);
-            sb.Append("[");
+            int len = 2;
 
             for (int i = 0; i < bytes.Length; ++i)
             {
                 if (i > 0)
                 {
-                    sb.Append(",");
+                    ++len; //for comma
                 }
-                sb.Append(bytes[i]);
+
+                byte val = bytes[i];
+                if (val < 10)
+                {
+                    ++len;
+                }
+                else if (val < 100)
+                {
+                    len += 2;
+                }
+                else
+                {
+                    len += 3;
+                }
             }
 
-            sb.Append("]");
-            return sb.ToString();
+            char[] chars = new char[len];
+
+            chars[0] = '[';
+            chars[len-1] = ']';
+            int pos = 1;
+            
+            foreach(var val in bytes)
+            {
+                if (pos > 1)
+                {
+                    chars[pos++] = ',';
+                }
+                string str = BYTES_MAP[val];
+                foreach(var ch in str)
+                {
+                    chars[pos++] = ch;
+                }
+            }
+     
+            return new String(chars);
         }
     }
 }
